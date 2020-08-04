41bd516366144c650d19b66a29b3c9657896588f
Justin Miller <2364004+Blu-J@users.noreply.github.com>
2020-08-03 20:13:55 -0600

    Update either.ts

--- src/either.ts
+++ src/either.ts
@@ -1,6 +1,5 @@
 import { MonadUnion } from "./monad";
 
-
 export class Either<L, R> extends MonadUnion<{ left: L; right: R }, "right"> {
   static left = <L>(value: L): Either<L, any> =>
     MonadUnion.of("right", "left", value) as Either<L, any>;
