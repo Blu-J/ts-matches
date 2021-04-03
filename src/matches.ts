8de2b12998e1d2f2b634d5f6b65581ce9811f254
Jade H <2364004+Blu-J@users.noreply.github.com>
2021-04-03 10:10:00 -0600

    chore: Matching does better type checking

--- src/matches.ts
+++ src/matches.ts
@@ -24,49 +24,16 @@ import {
   literals,
 } from "./parsers";
 
-export interface ChainMatches<Values, OutcomeType = never> {
-  when<Args extends Values extends never ? never : unknown[]>(
-    ...args: Args
-  ): ChainMatches<
-    Exclude<Values, WhenInput<Args>>,
-    OutcomeType | WhenOutput<Args>
-  >;
-  defaultTo<B>(value: B): B | OutcomeType;
-  defaultToLazy<B>(getValue: () => B): B | OutcomeType;
-  unwrap(): OutcomeType;
-}
 export { Parser as Validator, ValidatorError };
 
 // prettier-ignore
-export type ValueOrFunction<A, Input> = 
-A extends (() => infer B) ? B :
-  A extends ((input: infer InputB) => infer B) ? (
-    InputB extends Input ? B : never) :
-  A extends Function ? never :
-  A
-// prettier-ignore
-export type ParserOrLiteral<A> = 
-  A extends Parser<unknown, infer B> ? B :
-  A
-// prettier-ignore
-export type WhenOutput<A extends unknown[]> = 
-  A extends [] ? never :
-  A extends [infer B] ? (
-    ValueOrFunction<B, never>
-  ) :
-  A extends [infer B, infer C] ? ValueOrFunction<C, ParserOrLiteral<B>> :
-  A extends [infer B, infer C, infer D] ? ValueOrFunction<D, ParserOrLiteral<B> | ParserOrLiteral<C>> :
-  A extends [infer B, infer C,...infer Betweens, infer D] ? WhenOutput<[ParserOrLiteral<B> | ParserOrLiteral<C>, Betweens,D]> :
-  never
-// prettier-ignore
-export type WhenInput<A extends unknown[]> = 
-    A extends [] ? never :
-    A extends [infer B] ? any :
-    A extends [infer B, unknown] ? ParserOrLiteral<B> :
-    A extends [infer B, infer C, unknown] ? ParserOrLiteral<B> | ParserOrLiteral<C> :
-    A extends [infer B, infer C,...infer Betweens, unknown] ? WhenInput<[ParserOrLiteral<B> | ParserOrLiteral<C>, Betweens,unknown]> :
-    never
+export type ValueOrFunction<In, Out> =
+ | ((a: In) => Out)
+ | (() => Out)
+ | Out
 
+// prettier-ignore
+export type ParserOrLiteral<A> = ExtendsSimple<A> | Parser<unknown, A>
 export type ExtendsSimple<A> = A extends
   | string
   | number
@@ -75,14 +42,25 @@ export type ExtendsSimple<A> = A extends
   | undefined
   ? A
   : never;
+
+export type WhenArgs<In, Out> =
+  | [ValueOrFunction<In, Out>]
+  | [...ParserOrLiteral<In>[], ValueOrFunction<In, Out>];
+
+export interface ChainMatches<In, OutcomeType = never> {
+  when<A, B>(
+    ...args: In extends never ? never : WhenArgs<A, B>
+  ): ChainMatches<Exclude<In, A>, OutcomeType | B>;
+  defaultTo<B>(value: B): B | OutcomeType;
+  defaultToLazy<B>(getValue: () => B): B | OutcomeType;
+  unwrap(): OutcomeType;
+}
+
 class Matched<Ins, OutcomeType> implements ChainMatches<Ins, OutcomeType> {
   constructor(private value: OutcomeType) {}
-  when<Args extends Ins extends never ? never : unknown[]>(
-    ..._args: Args
-  ): ChainMatches<
-    Exclude<Ins, WhenInput<Args>>,
-    OutcomeType | WhenOutput<Args>
-  > {
+  when<A, B>(
+    ...args: WhenArgs<A, B>
+  ): ChainMatches<Exclude<Ins, A>, OutcomeType | B> {
     return this as any;
   }
   defaultTo<B>(_defaultValue: B) {
@@ -99,12 +77,9 @@ class Matched<Ins, OutcomeType> implements ChainMatches<Ins, OutcomeType> {
 class MatchMore<Ins, OutcomeType> implements ChainMatches<Ins, OutcomeType> {
   constructor(private a: unknown) {}
 
-  when<Args extends Ins extends never ? never : unknown[]>(
-    ...args: Args
-  ): ChainMatches<
-    Exclude<Ins, WhenInput<Args>>,
-    OutcomeType | WhenOutput<Args>
-  > {
+  when<A, B>(
+    ...args: WhenArgs<A, B>
+  ): ChainMatches<Exclude<Ins, A>, OutcomeType | B> {
     const [outcome, ...matchers] = args.reverse();
     const me = this;
     const parser = matches.some(
@@ -113,7 +88,7 @@ class MatchMore<Ins, OutcomeType> implements ChainMatches<Ins, OutcomeType> {
       )
     );
     return parser.parse(this.a, {
-      parsed(value: unknown) {
+      parsed(value: any) {
         if (outcome instanceof Function) {
           return new Matched(outcome(value));
         }
