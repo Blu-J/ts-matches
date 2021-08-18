b2b71a34d9be458161c0525d2d5a94f197783d18
Justin Miller <justin.miller@saltlending.com>
2021-08-18 16:58:01 -0600

    chore: Add in better exports

--- src/matches.ts
+++ src/matches.ts
@@ -23,9 +23,9 @@ import {
   dictionary,
   literals,
 } from "./parsers";
-import { ParserNames } from "./parsers/interfaces";
+export { IParser, ParserNames } from "./parsers/interfaces";
 
-export { Parser as Validator, ValidatorError, ParserNames };
+export { Parser as Validator, ValidatorError };
 
 // prettier-ignore
 export type ValueOrFunction<In, Out> =
